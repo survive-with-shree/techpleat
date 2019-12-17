73d86658e6a150200d03720d4427d3e8ada53472
Devendra Parhate <devendraparhate@gmail.com>
2019-12-17 22:00:23 +0000

    Initial React setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# techpleat
-techpleat org :)
---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Interactive from 'react-interactive';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import ExampleComponent from './ExampleComponent';
+import PageNotFound from './PageNotFound';
+import Breadcrumbs from './Breadcrumbs';
+import s from '../styles/app.style';
+
+export default function App() {
+  return (
+    <div style={s.root}>
+      <h1 style={s.title}>Single Page Apps for GitHub Pages</h1>
+      <Interactive
+        as="a"
+        href="https://github.com/survive-with-shree/techpleat"
+        style={s.repoLink}
+        {...s.link}
+      >https://github.com/survive-with-shree/techpleat</Interactive>
+
+      <nav style={s.breadcrumbs}>
+        <Breadcrumbs />
+      </nav>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/example" component={ExampleComponent} />
+        <Route component={PageNotFound} />
+      </Switch>
+
+      <div style={s.creditLine}>
+        <Interactive
+          as="a"
+          href="https://techpleat.com/"
+          interactiveChild
+          focus={{}}
+          touchActive={{}}
+          touchActiveTapOnly
+        >
+          Code and concept by <span {...s.childLink}>Rafael Pedicini</span>
+        </Interactive>
+      </div>
+    </div>
+  );
+}
