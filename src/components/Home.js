f518ba89ae61234da8b57d3af678d2c1b907f1ee
Devendra Parhate <devendraparhate@gmail.com>
2019-12-22 13:36:39 +0000

    Added category

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,30 +16,14 @@ export default function Home() {
   return (
     <div>
       <p style={s.p}>
-        This is an example single page app built
-        with React and React&nbsp;Router using {' '}
-        <Code>BrowserRouter</Code>. Navigate with the links below and
-        refresh the page or copy/paste the url to test out the redirect
-        functionality deployed to overcome GitHub&nbsp;Pages incompatibility
-        with single page apps (like this one).
-      </p>
-      <p style={s.p}>
-        Please see the {repoReadmeLink('repo readme')} for instructions on how to
-        use this boilerplate to deploy your own single page app using GitHub Pages.
+        Checkout TeachPleat's {repoReadmeLink('GitHub repo')} for instructions to contribute and support our opensource platform.
       </p>
       <div style={s.pageLinkContainer}>
         <Interactive
           as={Link}
           {...s.link}
-          to="/example"
-        >Example page</Interactive>
-      </div>
-      <div style={s.pageLinkContainer}>
-        <Interactive
-          as={Link}
-          {...s.link}
-          to="/example/two-deep?field1=foo&field2=bar#boom!"
-        >Example two deep with query and hash</Interactive>
+          to="/category?id=mobile"
+        >Mobile</Interactive>
       </div>
     </div>
   );
