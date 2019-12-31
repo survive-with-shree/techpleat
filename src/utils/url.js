4cc757a51de169926c7bbf4f678149dd76bd7793
Devendra Parhate <devendraparhate@gmail.com>
2019-12-31 01:01:53 +0000

    Point doc source to techpleat

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,2 @@
-export const base = "https://raw.githubusercontent.com/survive-with-shree/techpleat/master/"
+export const base = "https://raw.githubusercontent.com/techpleat/techpleat/master/"
 export const docs = base + "docs/"
